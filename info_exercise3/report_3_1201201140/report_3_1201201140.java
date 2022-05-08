dc1236a06733f8d255f21a74314bafaa4839e4cd
yagikota <69202609+yagikota@users.noreply.github.com>
2022-05-08 22:01:35 +0900

    2022/5/8

--- info_exercise3/report_3_1201201140/report_3_1201201140.java
+++ info_exercise3/report_3_1201201140/report_3_1201201140.java
@@ -1,8 +1,16 @@
+// 1201201140
+// 八木洸太
+
 // ファイル読み込みのためのimport
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.Math;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Arrays;
+
 
 public class report_3_1201201140 {
 	// double型2次元配列を表示する関数
@@ -23,10 +31,78 @@ public class report_3_1201201140 {
 		System.out.println();
 	}
 
+	// Comparableインターフェースを実装する
+	public static class Sort implements Comparable<Sort> {
+		double first;
+		int second;
+
+		public Sort(double first, int second) {
+			this.first = first;
+			this.second = second;
+		}
+		public int compareTo(Sort num) {
+			//第一要素の値が同じ場合に、第二要素で昇順にソートする処理
+			if (this.first == num.first) {
+				return 1;
+			} else {
+				return (int)(this.first - num.first);
+			}
+		}
+	}
+
+	// xとyの距離を算出
+	public static double getDist(double[] x, double[] y) {
+		double pow_dist = 0.0;
+		for (int i = 0; i < x.length; i++) {
+			pow_dist += Math.pow(x[i] - y[i], 2);
+		}
+		return Math.sqrt(pow_dist);
+	}
+
+	public static String[] getKthNearestIris(double[][] X, String[] y, int k, double[] x) {
+		// Sort[] sort = new Sort[X.length];
+		for (int i = 0; i < X.length; i++) {
+			double dist = getDist(X[i], x);
+			sort[i] = new Sort(dist, i);
+		}
+		Arrays.sort(sort);
+
+		// ソートしたマップからk個だけ抽出
+		String[] irisList = new String[k];
+		for(int i = 0; i < k; i++) {
+			irisList[i] = y[sort[i].second];
+		}
+		return irisList;
+	}
+
+	// 出現回数が最大の要素を返す
+	static String mostFrequent(String arr[])
+	{
+		Arrays.sort(arr);
+		int max_count = 1;
+		String res = arr[0];
+		int curr_count = 1;
+
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] == arr[i - 1])
+				curr_count++;
+			else
+				curr_count = 1;
+
+			if (curr_count > max_count) {
+				max_count = curr_count;
+				res = arr[i - 1];
+			}
+		}
+		return res;
+	}
+
 	// k近傍法
 	// X, yを訓練データとし，入力xに対しk近傍法を実行し，予測結果を返す．
 	public static String knn(double[][] X, String[] y, int k, double[] x) {
 		// ここを適切に完成させる
+		String[] irisList = getKthNearestIris(X, y, k, x);
+		return mostFrequent(irisList);
 	}
 
 	public static void main(String[] args) {
@@ -47,8 +123,18 @@ public class report_3_1201201140 {
 
 			// ここで X と y にデータを格納する (ここを適切に完成させる)
 			// 何らかの処理
+			int i = 0;
 			while ((row = bufferedReader.readLine()) != null) {
 				// 何らかの処理
+				if (!(row.isEmpty())){
+					String row_list[] = row.split(",");
+					X[i][0] = Double.parseDouble(row_list[0]);
+					X[i][1] = Double.parseDouble(row_list[1]);
+					X[i][2] = Double.parseDouble(row_list[2]);
+					X[i][3] = Double.parseDouble(row_list[3]);
+					y[i] = row_list[4];
+					i++;
+				};
 			}
 			fileReader.close(); // ファイルを閉じてリソースを開放する
 		} catch (IOException e) {
@@ -63,7 +149,26 @@ public class report_3_1201201140 {
 		int k = 5;
 
 		// 以下でleave-one-out交差検証法でk近傍法の性能を確認する．(ここを適切に完成させる)
+		int correctCounter = 0;
+		for (int i = 0; i < n; i++) {
+			double[] x = X[i];
 
-
+			// Xとyを再定義
+			double[][] newX = new double[n-1][m-1];
+			double[] newy = new double[n-1]
+			for (int j = 0; j < n-1; j++) {
+				if (j == i) {
+					continue;
+				}
+				newX[j] = X[j];
+				newy[j] = y[j];
+			}
+			String iris = knn(newX, newy, k, x);
+			if (iris == y[i]) {
+				correctCounter++;
+			}
+		}
+		double prediction = (double)correctCounter / (double)n;
+		System.out.println("Prediction Accuracy = " + prediction);
 	}
 }
---
+++ info_exercise3/report_3_1201201140/tempCodeRunnerFile.java
@@ -0,0 +1,5 @@
+
+	// // X, yを訓練データとし，入力xに対しk近傍法を実行し，予測結果を返す．
+	// public static String knn(double[][] X, String[] y, int k, double[] x) {
+	// 	// ここを適切に完成させる
+	// }
