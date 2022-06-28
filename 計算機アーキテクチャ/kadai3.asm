6efd805867b47ae1e6acbfd02a4556c62de8b4c3
yagikota <69202609+yagikota@users.noreply.github.com>
2022-06-29 00:00:49 +0900

    計算機アーキテクチャ

---
+++ 計算機アーキテクチャ/kadai3.asm
@@ -0,0 +1,47 @@
+.data
+str: .space 16
+
+.text
+main:
+	# read_string
+	li $v0, 8
+	li $a1, 16
+	la $a0, str
+	syscall
+
+loop:
+	lb	$t1, str($t0)
+ 	beq	$t1, 0, exit
+
+	# ~ @
+	blt	$t1, 'A', not_change_case
+	# A ~ Z
+	blt 	$t1, '[', to_lower_case
+ 	# [ ~ `
+ 	blt	$t1, 'a', not_change_case
+ 	# a ~ z
+	blt 	$t1, '{', to_upper_case
+	# { ~
+	bgt	$t1, 'z', not_change_case
+
+not_change_case: 
+	addi $t0, $t0, 1
+	j loop
+to_upper_case:
+	sub 	$t1, $t1, 32
+	sb 	$t1, str($t0)
+	addi	$t0, $t0, 1
+	j loop	
+to_lower_case:
+	add 	$t1, $t1, 32
+	sb	$t1, str($t0)
+	addi	$t0, $t0, 1
+	j loop
+exit:
+	# print_string
+	li	$v0, 4
+	la	$a0, str
+	syscall
+
+	li	$v0, 10
+	syscall
